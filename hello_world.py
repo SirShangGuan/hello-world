30ea333aedfdff5d9bcb0064753ab62b593d4e3b
SirShangGuan <guanfenglin@163.com>
2022-03-23 21:45:41 +0800

    Try more codes for python3 interpreter

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,25 @@
 #!/opt/homebrew/bin/python3
 
+from pyuvm import *
+from allpairspy import AllPairs
+
+
+parameters = [
+    ["Brand X", "Brand Y"],
+    ["98", "NT", "2000", "XP"],
+    ["Internal", "Modem"],
+    ["Salaried", "Hourly", "Part-Time", "Contr."],
+    [6, 10, 15, 30, 60],
+]
+# sample parameters are is taken from
+# http://www.stsc.hill.af.mil/consulting/sw_testing/improvement/cst.html
+
+print("PAIRWISE:")
+for i, pairs in enumerate(AllPairs(parameters)):
+    print("{:2d}: {}".format(i, pairs))
+
+my_object = uvm_object("my_object")
+type(my_object)
+
+print("object name:", my_object.get_name())
 print("Hello World 2 !!!!")
